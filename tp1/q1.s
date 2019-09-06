70bc1c7603a756f9c3f0abb819746bd2b23fae08
guilhermebezerrads <40393853+guilhermebezerrads@users.noreply.github.com>
2019-09-06 10:11:08 -0300

    Simplificação de código e atualização na documentação
    - Foi removido o salvamento na memória de registradores que são temporários
    - Link do github adicionado na documentação

--- tp1/q1.s
+++ tp1/q1.s
@@ -3,15 +3,12 @@ argumento: .word 7
 
 .text
 main:
-	lw x10, argumento
-    call Impar_Par
+	lw x10, argumento #recebe o argumento em x10
+    call Impar_Par 
     jal Fim
 
 Impar_Par:
-	addi sp, sp, -4
-	sw x5, 0(sp)
-	addi x5, x0, 2
-	rem x10, x10, x5
-	lw x5, 0(sp) 
+	addi x5, x0, 2 #x5 = 2
+	rem x10, x10, x5 #x10 = argumento mod 2
 	jalr x0, 0(x1)
 Fim:
